230a1da55a67e210e975c979a94cf056e101518e
Alexey Bogdanov <lex.nox@gmail.com>
2019-01-14 01:03:37 +0300

    Added examples for chapter 1 First Steps part 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,19 @@
 module Main where
 
-import Lib
+    import Lib
+    
+    main :: IO ()
+    
+    -- Example 1
+    double x = x + x
+    quadruple x = double (double x)
+    
+    -- uncomment for ex 1
+    -- main = print (show (take (double 2) [1, 2, 3, 4, 5, 6])) 
 
-main :: IO ()
+    -- Example 2
+    factorial n = product [1..n]
+    average ns = sum ns `div` length ns
 
-double x = x + x
-
-quadruple x = double (double x)
-
-main = print (show (take (double 2) [1, 2, 3, 4, 5, 6])) 
+    main = print ((show (factorial 10)) ++ " " ++ show (average [1, 2, 3, 4, 5]))
+    
